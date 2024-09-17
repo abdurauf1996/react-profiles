1b1725b2fe15a322d06d4984e87bfb9d7567996e
abdurauf1996 <vakhobovabdurauf@gmail.com>
2024-09-17 17:03:23 +0900

    creat a profile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Avatar from "./Avatar";
 function App() {
+  const url = "https://reqres.in/img/faces/7-image.jpg";
+  const name = "Michel Lawson";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Avatar name={name} url={url} />
     </div>
   );
 }
---
+++ src/Avatar.js
@@ -0,0 +1,14 @@
+import "./Avatar.css";
+
+function Avatar({ name, url }) {
+  return (
+    <img
+      className="profile-avatar"
+      width="128"
+      height="128"
+      alt={name}
+      src={url}
+    />
+  );
+}
+export default Avatar;
