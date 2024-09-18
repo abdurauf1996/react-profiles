ba94969fa9876bf7e32942386e56b6f737935c15
abdurauf1996 <vakhobovabdurauf@gmail.com>
2024-09-18 18:31:37 +0900

    react hooks - useEffect() and useState()

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import { useState } from "react";
 import "./App.css";
-
+import Paging from "./Paging";
 import Profiles from "./Profiles";
 import load from "./users";
 import { useEffect } from "react";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
   useEffect(() => {
-    load().then((data) => setUsers(data));
-  }, []);
+    load(currentPage).then((result) => {
+      setUsers(result.data);
+      setCurrentPage(result.page);
+      setTotalPages(result.total_pages);
+    });
+  }, [currentPage]);
 
   return (
     <div className="App">
+      <Paging
+        currentPage={currentPage}
+        totalPages={totalPages}
+        setCurrentPage={setCurrentPage}
+      />
       <Profiles users={users} />
     </div>
   );
--- src/users.js
+++ src/users.js
@@ -1,9 +1,11 @@
-function load() {
-  return fetch("https://reqres.in/api/users")
-    .then((response) => response.json())
-    .then((result) => result.data)
-    .catch((err) => {
-      console.log(err);
-    });
+function load(page = 1) {
+  return (
+    fetch(`https://reqres.in/api/users?page=${page}`)
+      .then((response) => response.json())
+      // .then((result) => result.data)
+      .catch((err) => {
+        console.log(err);
+      })
+  );
 }
 export default load;
